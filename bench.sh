af3f68b2d5a120019a6b53a54d45905ad3084210
Steven Costiou <steven.costiou@kloum.io>
2020-03-11 12:40:05 +0100

    Update bench.sh
    Fixing computation and printing.

--- bench.sh
+++ bench.sh
@@ -1,7 +1,7 @@
-START=$(date +%s.%N)
-for i in {1..10};
+START=$(date +%s)
+for i in {1..1};
 do ../../vms/70-x64/Pharo.app/Contents/MacOS/pharo --headless Miners_JOTBenchmarks.image bench.st;
 done
-END=$(date +%s.%N)
-DIFF=$(echo "Benchmark finished, elapsede time: $END - $START" | bc)
-
+END=$(date +%s)
+let "DIFF=$END - $START"
+echo -e "\nBenchmarks ended in $DIFF seconds"
