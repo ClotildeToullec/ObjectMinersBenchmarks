80e3d3604c35b70aa6be9655e4c94aca5bbd0239
Steven Costiou <steven.costiou@kloum.io>
2020-03-11 12:08:36 +0100

    Update bench.sh
    Added the time it takes

--- bench.sh
+++ bench.sh
@@ -1,3 +1,7 @@
+START=$(date +%s.%N)
 for i in {1..10};
 do ../../vms/70-x64/Pharo.app/Contents/MacOS/pharo --headless Miners_JOTBenchmarks.image bench.st;
 done
+END=$(date +%s.%N)
+DIFF=$(echo "Benchmark finished, elapsede time: $END - $START" | bc)
+
