9a74d09266a2b7103b4d2ff62a40b88dbfd53315
Steven Costiou <steven.costiou@kloum.io>
2020-03-13 20:48:18 +0100

    Update bench.sh

--- scripts/bench.sh
+++ scripts/bench.sh
@@ -3,9 +3,10 @@ SCRIPTVERSION=$1
 SCRIPTEXT=".st"
 SCRIPT=$SCRIPTNAME$SCRIPTVERSION$SCRIPTEXT
 START=$(date +%s)
-for i in {1..1};
-do ../../vms/70-x64/Pharo.app/Contents/MacOS/pharo --headless Miners_JOTBenchmarks.image $SCRIPT;
-done
+#for i in {1..1};
+#do ../../vms/70-x64/Pharo.app/Contents/MacOS/pharo --headless Miners_RFTest $SCRIPT;
+#done
+../../vms/70-x64/Pharo.app/Contents/MacOS/pharo --headless Miners_RFTest $SCRIPT;
 END=$(date +%s)
 let "DIFF=$END - $START"
 echo -e "\nBenchmarks ended in $DIFF seconds"
